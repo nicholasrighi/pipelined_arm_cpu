66085cf3a3cc9fac850aa56091e98d78f6dd24f1
nicholasrighi <nicholasrighi@gmail.com>
2021-03-18 19:05:18 -0700

    reverted h detector

--- verilog_src/hazard_detector.sv
+++ verilog_src/hazard_detector.sv
@@ -5,7 +5,6 @@ module hazard_detector(
                         input logic [ADDR_WIDTH-1:0] dest_addr_reg_EXE_i,
                         input logic [ADDR_WIDTH-1:0] source_reg_1_DECODE_i,
                         input logic [ADDR_WIDTH-1:0] source_reg_2_DECODE_i,
-                        input logic [ADDR_WIDTH-1:0] source_reg_3_DECODE_i,
     
                         output stall_pipeline_sig stall_pipeline_o,
                         output logic              hazard_detector_invaidate_o
@@ -16,8 +15,7 @@ module hazard_detector(
         // always occur together. This is important because if mem_read is asserted but reg_write isn't asserted, 
         // then the hazard detector can stall when there's no need to. This doesn't effect the functionality of the 
         // cpu, but it will negatively impact performance.
-        if (mem_read_EXE_i && ( (dest_addr_reg_EXE_i == source_reg_1_DECODE_i) || (dest_addr_reg_EXE_i == source_reg_2_DECODE_i) 
-            || (dest_addr_reg_EXE_i == source_reg_3_DECODE_i) ))
+        if (mem_read_EXE_i && ( (dest_addr_reg_EXE_i == source_reg_1_DECODE_i) || (dest_addr_reg_EXE_i == source_reg_2_DECODE_i)))
 
             stall_pipeline_o = STALL_PIPELINE;
         else
